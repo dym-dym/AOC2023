53ba2c325d1b234b809e256d7d655b78c291af57
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2023-12-01 14:39:28 +0100

    day 1 part 1

--- day-01/src/part1.rs
+++ day-01/src/part1.rs
@@ -1,7 +1,30 @@
+use rayon::prelude::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
+
 use crate::custom_error::AocError;
 
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    Ok("".to_string())
+pub fn process(input: &str) -> miette::Result<i64, AocError> {
+    let mut res = vec![];
+
+    input
+        .trim_end()
+        .split('\n')
+        .map(|x| {
+            x.trim_end()
+                .chars()
+                .filter(|x| x.is_numeric())
+                .collect::<Vec<char>>()
+        })
+        .map(|x| {
+            let mut res = String::from(x[0]);
+            res.push(x[x.len() - 1]);
+            res
+        })
+        .collect::<Vec<String>>()
+        .par_iter()
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect_into_vec(&mut res);
+
+    Ok(res.par_iter().sum())
 }
 
 #[cfg(test)]
@@ -10,8 +33,11 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!(142, process(input)?);
         Ok(())
     }
 }
